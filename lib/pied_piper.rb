b225dc1f3d8850bee65aa9d47263c17eb3944d40
Christoph Weegen <christophweegen@googlemail.com>
2019-01-24 11:55:41 +0100

    First commit / generate gem

---
+++ lib/pied_piper.rb
@@ -0,0 +1,5 @@
+require "pied_piper/version"
+
+module PiedPiper
+  # Your code goes here...
+end
---
+++ spec/pied_piper_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe PiedPiper do
+  it "has a version number" do
+    expect(PiedPiper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
